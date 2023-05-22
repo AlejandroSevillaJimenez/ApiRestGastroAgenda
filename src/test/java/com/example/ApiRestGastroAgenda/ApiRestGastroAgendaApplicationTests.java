6cdd7c138f1cae7e6ae42f1c321217b1e7e1f57c
AlejandroSevillaJimenez <75426906+AlejandroSevillaJimenez@users.noreply.github.com>
2023-05-22 12:18:13 +0200

    Primer commit

---
+++ src/test/java/com/example/ApiRestGastroAgenda/ApiRestGastroAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestGastroAgenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestGastroAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
