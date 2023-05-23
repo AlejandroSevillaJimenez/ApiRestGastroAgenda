6bf76cebbfd4128b7a761d24f787766efbe864d3
AlejandroSevillaJimenez <75426906+AlejandroSevillaJimenez@users.noreply.github.com>
2023-05-23 12:50:47 +0200

    Añado existeUsuarioRegistrar

--- src/main/java/com/example/ApiRestGastroAgenda/service/impl/RestaurantesServiceImpl.java
+++ src/main/java/com/example/ApiRestGastroAgenda/service/impl/RestaurantesServiceImpl.java
@@ -5,9 +5,11 @@ import com.example.ApiRestGastroAgenda.model.Usuario;
 import com.example.ApiRestGastroAgenda.repository.RestaurantesRepository;
 import com.example.ApiRestGastroAgenda.service.RestaurantesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RestaurantesServiceImpl implements RestaurantesService {
 
     @Autowired
